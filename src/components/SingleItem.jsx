d6a7269af0b3a3b2013f7a56aada06dda292110c
Alireza Ghasemi <agh102724@gmail.com>
2024-12-29 23:18:45 +0330

    add-lineThrough

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 function SingleItem({ item }) {
+  const [checked, setChecked] = useState(false);
+  const lineThrough = checked ? "line-through" : "";
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
   return (
     <div className="flex flex-row w-full mb-5 justify-between items-center text-sm">
       <div className="flex flex-row">
-        <input type="checkbox" />
-        <h5 className="ml-3 font-medium">{item.name}</h5>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        <h5 className={`ml-3 font-medium ${lineThrough}`}>{item.name}</h5>
       </div>
       <button className="bg-gray-900 px-1 hover:bg-blue-400 transition-all duration-300 text-white rounded">
         Delete
